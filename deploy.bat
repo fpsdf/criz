880a1baa3a2104d476bc795264ba2fd3fd0f72ed
fpsdf <franciscopauloferreira@gmail.com>
2026-03-07 12:01:45 +0000

    update

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 cd "C:\Users\franc\Local Sites\casa-oficina-da-ribeira-do-criz\app\public\public_static"
 git init
 git remote remove origin
-git remote add origin https://github.com/fpsdf/criz
+git remote add origin https://github.com/fpsdf/criz.git
 git add .
 git commit -m "update"
-git push origin main --force
+git push origin master:main --force
 pause
